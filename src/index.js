c266b1ff87cfc946b6d5f8d2e8b0a509ce372bd5
Henrik Nørgaard <menneskefernis@gmail.com>
2020-09-03 13:31:45 +0200

    Add a bunch of loaders and output handling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import _ from 'lodash';
-import './style.css';
+import printMe from './print.js';
+// import './style.css';
+// // import Icon from './my-image.jpg';
+// // import Data from './data.xml';
+// // import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, now imported by this script
+
+  // element.classList.add('hello');
+  
+  // const myIcon = new Image();
+  // myIcon.src = Icon;
+
+  // element.appendChild(myIcon);
+
+  // console.log(Data);
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
